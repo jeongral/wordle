37e13dea7917044906abea9a4041e9d294f71068
Esther Lee <jeongral@uci.edu>
2024-03-17 23:40:29 -0700

    Added loading screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useState, useEffect } from "react";
 import Main from "./components";
+import Loading from "./components/loading";
 
 export default function App() {
-  return (
-    <Main />
-  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 2500)
+  }, []);
+  return (<div>
+    {isLoading ? (
+      <Loading />
+    ) : (
+      <Main />
+    )}
+  </div>);
 }
---
+++ src/components/loading.js
@@ -0,0 +1,8 @@
+export default function Loading() {
+    return (
+    <div className="loading-container">
+        <div className="loading-progress">
+            <div className="loading-bar"></div>
+        </div>
+    </div>)
+}
